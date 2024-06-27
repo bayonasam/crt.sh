3eed073d008c339e29b90251cd618c8fc6ff1f3e
bayonasam <bayonasam@gmail.com>
2024-06-27 17:04:55 +0200

    Update crt.go

--- crt.go
+++ crt.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -36,16 +37,29 @@ func main() {
                           /____/ @TaurusOmar_
 	`)
 
-	if len(os.Args) != 2 {
-		fmt.Printf("Use: %s <domain>\n", os.Args[0])
+	// Define command line flags
+	domain := flag.String("d", "", "Domain to scan")
+	output := flag.String("o", "", "Output file path")
+	flag.Parse()
+
+	// Check if domain is provided
+	if *domain == "" {
+		fmt.Printf("Use: %s -d <domain> [-o <output file path>]\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	domain := os.Args[1]
-	resultDir := "result_directory"
-	crtURL := fmt.Sprintf("https://crt.sh/?q=%%25.%s&output=json", domain)
+	// Default output directory if not provided
+	if *output == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+		*output = filepath.Join(homeDir, "result_directory", fmt.Sprintf("%s.crt.txt", *domain))
+	}
 
-	fmt.Printf("Scanning for domain: %s...\n", domain)
+	crtURL := fmt.Sprintf("https://crt.sh/?q=%%25.%s&output=json", *domain)
+
+	fmt.Printf("Scanning for domain: %s...\n", *domain)
 
 	go spinner(100 * time.Millisecond)
 
@@ -79,24 +93,18 @@ func main() {
 
 	sort.Strings(uniqueSubdomains)
 
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
-
-	resultDir = filepath.Join(homeDir, resultDir)
-	if _, err := os.Stat(resultDir); os.IsNotExist(err) {
-		err = os.Mkdir(resultDir, 0755)
+	outputDir := filepath.Dir(*output)
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		err = os.MkdirAll(outputDir, 0755)
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 		}
 	}
 
-	crtOutput := filepath.Join(resultDir, fmt.Sprintf("%s.crt.txt", domain))
-	err = ioutil.WriteFile(crtOutput, []byte(strings.Join(uniqueSubdomains, "\n")), 0644)
+	err = ioutil.WriteFile(*output, []byte(strings.Join(uniqueSubdomains, "\n")), 0644)
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 
-	fmt.Printf("\rScan completed. Results saved in %s\n", crtOutput)
+	fmt.Printf("\rScan completed. Results saved in %s\n", *output)
 }
